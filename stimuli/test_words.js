c19a7fedff8ea4ff4f44dc6fd04f584af2a45097
Jackie Spear <jackiespear44@gmail.com>
2021-06-16 10:57:04 -0500

    uploading experiment

---
+++ stimuli/test_words.js
@@ -0,0 +1,242 @@
+testwords = [
+{"test1":"<p style='font-size: 48px;'>HELL</p>","data1":{"type":"HF","category":"AB","word":"HELL"},
+"test2":"<p style='font-size: 48px;'>RIVER</p>","data2":{"type":"HF","category":"CN","word":"RIVER"}
+},
+{"test1":"<p style='font-size: 48px;'>INFLUENCE</p>","data1":{"type":"HF","category":"AB","word":"INFLUENCE"},
+"test2":"<p style='font-size: 48px;'>SUN</p>","data2":{"type":"HF","category":"CN","word":"SUN"}
+},
+{"test1":"<p style='font-size: 48px;'>COURSE</p>","data1":{"type":"HF","category":"AB","word":"COURSE"},
+"test2":"<p style='font-size: 48px;'>BALL</p>","data2":{"type":"HF","category":"CN","word":"BALL"}
+},
+{"test1":"<p style='font-size: 48px;'>PHILOSOPHY</p>","data1":{"type":"HF","category":"AB","word":"PHILOSOPHY"},
+"test2":"<p style='font-size: 48px;'>ROOM</p>","data2":{"type":"HF","category":"CN","word":"ROOM"}
+},
+{"test1":"<p style='font-size: 48px;'>VALUE</p>","data1":{"type":"HF","category":"AB","word":"VALUE"},
+"test2":"<p style='font-size: 48px;'>SPOKE</p>","data2":{"type":"HF","category":"CN","word":"SPOKE"}
+},
+{"test1":"<p style='font-size: 48px;'>NONE</p>","data1":{"type":"HF","category":"AB","word":"NONE"},
+"test2":"<p style='font-size: 48px;'>HEART</p>","data2":{"type":"HF","category":"CN","word":"HEART"}
+},
+{"test1":"<p style='font-size: 48px;'>CRISIS</p>","data1":{"type":"HF","category":"AB","word":"CRISIS"},
+"test2":"<p style='font-size: 48px;'>HOTEL</p>","data2":{"type":"HF","category":"CN","word":"HOTEL"}
+},
+{"test1":"<p style='font-size: 48px;'>MOVE</p>","data1":{"type":"HF","category":"AB","word":"MOVE"},
+"test2":"<p style='font-size: 48px;'>LETTER</p>","data2":{"type":"HF","category":"CN","word":"LETTER"}
+},
+{"test1":"<p style='font-size: 48px;'>BEST</p>","data1":{"type":"HF","category":"AB","word":"BEST"},
+"test2":"<p style='font-size: 48px;'>KEY</p>","data2":{"type":"HF","category":"CN","word":"KEY"}
+},
+{"test1":"<p style='font-size: 48px;'>TRUTH</p>","data1":{"type":"HF","category":"AB","word":"TRUTH"},
+"test2":"<p style='font-size: 48px;'>CORNER</p>","data2":{"type":"HF","category":"CN","word":"CORNER"}
+},
+{"test1":"<p style='font-size: 48px;'>EXTENT</p>","data1":{"type":"HF","category":"AB","word":"EXTENT"},
+"test2":"<p style='font-size: 48px;'>MONEY</p>","data2":{"type":"HF","category":"CN","word":"MONEY"}
+},
+{"test1":"<p style='font-size: 48px;'>APPEAR</p>","data1":{"type":"HF","category":"AB","word":"APPEAR"},
+"test2":"<p style='font-size: 48px;'>POST</p>","data2":{"type":"HF","category":"CN","word":"POST"}
+},
+{"test1":"<p style='font-size: 48px;'>AREA</p>","data1":{"type":"HF","category":"AB","word":"AREA"},
+"test2":"<p style='font-size: 48px;'>SHIP</p>","data2":{"type":"HF","category":"CN","word":"SHIP"}
+},
+{"test1":"<p style='font-size: 48px;'>EVIDENCE</p>","data1":{"type":"HF","category":"AB","word":"EVIDENCE"},
+"test2":"<p style='font-size: 48px;'>BANK</p>","data2":{"type":"HF","category":"CN","word":"BANK"}
+},
+{"test1":"<p style='font-size: 48px;'>BEGINNING</p>","data1":{"type":"HF","category":"AB","word":"BEGINNING"},
+"test2":"<p style='font-size: 48px;'>SCHOOL</p>","data2":{"type":"HF","category":"CN","word":"SCHOOL"}
+},
+{"test1":"<p style='font-size: 48px;'>RESPONSIBILITY</p>","data1":{"type":"HF","category":"AB","word":"RESPONSIBILITY"},
+"test2":"<p style='font-size: 48px;'>POOL</p>","data2":{"type":"HF","category":"CN","word":"POOL"}
+},
+{"test1":"<p style='font-size: 48px;'>STANDARD</p>","data1":{"type":"HF","category":"AB","word":"STANDARD"},
+"test2":"<p style='font-size: 48px;'>SECRETARY</p>","data2":{"type":"HF","category":"CN","word":"SECRETARY"}
+},
+{"test1":"<p style='font-size: 48px;'>HAD</p>","data1":{"type":"HF","category":"AB","word":"HAD"},
+"test2":"<p style='font-size: 48px;'>MEN</p>","data2":{"type":"HF","category":"CN","word":"MEN"}
+},
+{"test1":"<p style='font-size: 48px;'>PAID</p>","data1":{"type":"HF","category":"AB","word":"PAID"},
+"test2":"<p style='font-size: 48px;'>WIFE</p>","data2":{"type":"HF","category":"CN","word":"WIFE"}
+},
+{"test1":"<p style='font-size: 48px;'>REASON</p>","data1":{"type":"HF","category":"AB","word":"REASON"},
+"test2":"<p style='font-size: 48px;'>HAND</p>","data2":{"type":"HF","category":"CN","word":"HAND"}
+},
+{"test1":"<p style='font-size: 48px;'>CLAIM</p>","data1":{"type":"HF","category":"AB","word":"CLAIM"},
+"test2":"<p style='font-size: 48px;'>TYPHOON</p>","data2":{"type":"LF","category":"CN","word":"TYPHOON"}
+},
+{"test1":"<p style='font-size: 48px;'>MAKE</p>","data1":{"type":"HF","category":"AB","word":"MAKE"},
+"test2":"<p style='font-size: 48px;'>PICKLE</p>","data2":{"type":"LF","category":"CN","word":"PICKLE"}
+},
+{"test1":"<p style='font-size: 48px;'>REST</p>","data1":{"type":"HF","category":"AB","word":"REST"},
+"test2":"<p style='font-size: 48px;'>FOWL</p>","data2":{"type":"LF","category":"CN","word":"FOWL"}
+},
+{"test1":"<p style='font-size: 48px;'>DIFFERENCE</p>","data1":{"type":"HF","category":"AB","word":"DIFFERENCE"},
+"test2":"<p style='font-size: 48px;'>DOE</p>","data2":{"type":"LF","category":"CN","word":"DOE"}
+},
+{"test1":"<p style='font-size: 48px;'>ACT</p>","data1":{"type":"HF","category":"AB","word":"ACT"},
+"test2":"<p style='font-size: 48px;'>GIG</p>","data2":{"type":"LF","category":"CN","word":"GIG"}
+},
+{"test1":"<p style='font-size: 48px;'>APPROACH</p>","data1":{"type":"HF","category":"AB","word":"APPROACH"},
+"test2":"<p style='font-size: 48px;'>PLUM</p>","data2":{"type":"LF","category":"CN","word":"PLUM"}
+},
+{"test1":"<p style='font-size: 48px;'>MIND</p>","data1":{"type":"HF","category":"AB","word":"MIND"},
+"test2":"<p style='font-size: 48px;'>CRYPT</p>","data2":{"type":"LF","category":"CN","word":"CRYPT"}
+},
+{"test1":"<p style='font-size: 48px;'>LIFE</p>","data1":{"type":"HF","category":"AB","word":"LIFE"},
+"test2":"<p style='font-size: 48px;'>FOOTWEAR</p>","data2":{"type":"LF","category":"CN","word":"FOOTWEAR"}
+},
+{"test1":"<p style='font-size: 48px;'>CENTURY</p>","data1":{"type":"HF","category":"AB","word":"CENTURY"},
+"test2":"<p style='font-size: 48px;'>SURF</p>","data2":{"type":"LF","category":"CN","word":"SURF"}
+},
+{"test1":"<p style='font-size: 48px;'>LACK</p>","data1":{"type":"HF","category":"AB","word":"LACK"},
+"test2":"<p style='font-size: 48px;'>REFEREE</p>","data2":{"type":"LF","category":"CN","word":"REFEREE"}
+},
+{"test1":"<p style='font-size: 48px;'>WENT</p>","data1":{"type":"HF","category":"AB","word":"WENT"},
+"test2":"<p style='font-size: 48px;'>ICICLE</p>","data2":{"type":"LF","category":"CN","word":"ICICLE"}
+},
+{"test1":"<p style='font-size: 48px;'>STYLE</p>","data1":{"type":"HF","category":"AB","word":"STYLE"},
+"test2":"<p style='font-size: 48px;'>DIAL</p>","data2":{"type":"LF","category":"CN","word":"DIAL"}
+},
+{"test1":"<p style='font-size: 48px;'>FACT</p>","data1":{"type":"HF","category":"AB","word":"FACT"},
+"test2":"<p style='font-size: 48px;'>CLAW</p>","data2":{"type":"LF","category":"CN","word":"CLAW"}
+},
+{"test1":"<p style='font-size: 48px;'>SHORT</p>","data1":{"type":"HF","category":"AB","word":"SHORT"},
+"test2":"<p style='font-size: 48px;'>PROJECTILE</p>","data2":{"type":"LF","category":"CN","word":"PROJECTILE"}
+},
+{"test1":"<p style='font-size: 48px;'>SAID</p>","data1":{"type":"HF","category":"AB","word":"SAID"},
+"test2":"<p style='font-size: 48px;'>EARTHWORM</p>","data2":{"type":"LF","category":"CN","word":"EARTHWORM"}
+},
+{"test1":"<p style='font-size: 48px;'>LOW</p>","data1":{"type":"HF","category":"AB","word":"LOW"},
+"test2":"<p style='font-size: 48px;'>MOCCASIN</p>","data2":{"type":"LF","category":"CN","word":"MOCCASIN"}
+},
+{"test1":"<p style='font-size: 48px;'>BORN</p>","data1":{"type":"HF","category":"AB","word":"BORN"},
+"test2":"<p style='font-size: 48px;'>EVERGREEN</p>","data2":{"type":"LF","category":"CN","word":"EVERGREEN"}
+},
+{"test1":"<p style='font-size: 48px;'>EIGHT</p>","data1":{"type":"HF","category":"AB","word":"EIGHT"},
+"test2":"<p style='font-size: 48px;'>CUBE</p>","data2":{"type":"LF","category":"CN","word":"CUBE"}
+},
+{"test1":"<p style='font-size: 48px;'>BALANCE</p>","data1":{"type":"HF","category":"AB","word":"BALANCE"},
+"test2":"<p style='font-size: 48px;'>DAMSEL</p>","data2":{"type":"LF","category":"CN","word":"DAMSEL"}
+},
+{"test1":"<p style='font-size: 48px;'>PLAN</p>","data1":{"type":"HF","category":"AB","word":"PLAN"},
+"test2":"<p style='font-size: 48px;'>APRICOT</p>","data2":{"type":"LF","category":"CN","word":"APRICOT"}
+},
+{"test1":"<p style='font-size: 48px;'>HUMOUR</p>","data1":{"type":"LF","category":"AB","word":"HUMOUR"},
+"test2":"<p style='font-size: 48px;'>BATTLE</p>","data2":{"type":"HF","category":"CN","word":"BATTLE"}
+},
+{"test1":"<p style='font-size: 48px;'>SOBRIETY</p>","data1":{"type":"LF","category":"AB","word":"SOBRIETY"},
+"test2":"<p style='font-size: 48px;'>POET</p>","data2":{"type":"HF","category":"CN","word":"POET"}
+},
+{"test1":"<p style='font-size: 48px;'>INTEMPERANCE</p>","data1":{"type":"LF","category":"AB","word":"INTEMPERANCE"},
+"test2":"<p style='font-size: 48px;'>NOTE</p>","data2":{"type":"HF","category":"CN","word":"NOTE"}
+},
+{"test1":"<p style='font-size: 48px;'>IOTA</p>","data1":{"type":"LF","category":"AB","word":"IOTA"},
+"test2":"<p style='font-size: 48px;'>OIL</p>","data2":{"type":"HF","category":"CN","word":"OIL"}
+},
+{"test1":"<p style='font-size: 48px;'>WRACK</p>","data1":{"type":"LF","category":"AB","word":"WRACK"},
+"test2":"<p style='font-size: 48px;'>GUN</p>","data2":{"type":"HF","category":"CN","word":"GUN"}
+},
+{"test1":"<p style='font-size: 48px;'>USURP</p>","data1":{"type":"LF","category":"AB","word":"USURP"},
+"test2":"<p style='font-size: 48px;'>LADY</p>","data2":{"type":"HF","category":"CN","word":"LADY"}
+},
+{"test1":"<p style='font-size: 48px;'>QUICKEN</p>","data1":{"type":"LF","category":"AB","word":"QUICKEN"},
+"test2":"<p style='font-size: 48px;'>HALL</p>","data2":{"type":"HF","category":"CN","word":"HALL"}
+},
+{"test1":"<p style='font-size: 48px;'>SUBSTRATUM</p>","data1":{"type":"LF","category":"AB","word":"SUBSTRATUM"},
+"test2":"<p style='font-size: 48px;'>GIRL</p>","data2":{"type":"HF","category":"CN","word":"GIRL"}
+},
+{"test1":"<p style='font-size: 48px;'>WREST</p>","data1":{"type":"LF","category":"AB","word":"WREST"},
+"test2":"<p style='font-size: 48px;'>DOCTOR</p>","data2":{"type":"HF","category":"CN","word":"DOCTOR"}
+},
+{"test1":"<p style='font-size: 48px;'>ABDUCTION</p>","data1":{"type":"LF","category":"AB","word":"ABDUCTION"},
+"test2":"<p style='font-size: 48px;'>TEACHER</p>","data2":{"type":"HF","category":"CN","word":"TEACHER"}
+},
+{"test1":"<p style='font-size: 48px;'>INCREDULITY</p>","data1":{"type":"LF","category":"AB","word":"INCREDULITY"},
+"test2":"<p style='font-size: 48px;'>PLANE</p>","data2":{"type":"HF","category":"CN","word":"PLANE"}
+},
+{"test1":"<p style='font-size: 48px;'>EXHAUSTION</p>","data1":{"type":"LF","category":"AB","word":"EXHAUSTION"},
+"test2":"<p style='font-size: 48px;'>ARM</p>","data2":{"type":"HF","category":"CN","word":"ARM"}
+},
+{"test1":"<p style='font-size: 48px;'>DEXTERITY</p>","data1":{"type":"LF","category":"AB","word":"DEXTERITY"},
+"test2":"<p style='font-size: 48px;'>DRINK</p>","data2":{"type":"HF","category":"CN","word":"DRINK"}
+},
+{"test1":"<p style='font-size: 48px;'>PLENIPOTENTIARY</p>","data1":{"type":"LF","category":"AB","word":"PLENIPOTENTIARY"},
+"test2":"<p style='font-size: 48px;'>HOUSE</p>","data2":{"type":"HF","category":"CN","word":"HOUSE"}
+},
+{"test1":"<p style='font-size: 48px;'>ACIDITY</p>","data1":{"type":"LF","category":"AB","word":"ACIDITY"},
+"test2":"<p style='font-size: 48px;'>OFFICE</p>","data2":{"type":"HF","category":"CN","word":"OFFICE"}
+},
+{"test1":"<p style='font-size: 48px;'>CONVEYANCE</p>","data1":{"type":"LF","category":"AB","word":"CONVEYANCE"},
+"test2":"<p style='font-size: 48px;'>DINNER</p>","data2":{"type":"HF","category":"CN","word":"DINNER"}
+},
+{"test1":"<p style='font-size: 48px;'>SUFFIX</p>","data1":{"type":"LF","category":"AB","word":"SUFFIX"},
+"test2":"<p style='font-size: 48px;'>HEAD</p>","data2":{"type":"HF","category":"CN","word":"HEAD"}
+},
+{"test1":"<p style='font-size: 48px;'>ENDEAVOUR</p>","data1":{"type":"LF","category":"AB","word":"ENDEAVOUR"},
+"test2":"<p style='font-size: 48px;'>BED</p>","data2":{"type":"HF","category":"CN","word":"BED"}
+},
+{"test1":"<p style='font-size: 48px;'>DEFENCE</p>","data1":{"type":"LF","category":"AB","word":"DEFENCE"},
+"test2":"<p style='font-size: 48px;'>FIRE</p>","data2":{"type":"HF","category":"CN","word":"FIRE"}
+},
+{"test1":"<p style='font-size: 48px;'>UNCONCERN</p>","data1":{"type":"LF","category":"AB","word":"UNCONCERN"},
+"test2":"<p style='font-size: 48px;'>HORSE</p>","data2":{"type":"HF","category":"CN","word":"HORSE"}
+},
+{"test1":"<p style='font-size: 48px;'>IMPROPRIETY</p>","data1":{"type":"LF","category":"AB","word":"IMPROPRIETY"},
+"test2":"<p style='font-size: 48px;'>TORNADO</p>","data2":{"type":"LF","category":"CN","word":"TORNADO"}
+},
+{"test1":"<p style='font-size: 48px;'>SOLEMNITY</p>","data1":{"type":"LF","category":"AB","word":"SOLEMNITY"},
+"test2":"<p style='font-size: 48px;'>CROCODILE</p>","data2":{"type":"LF","category":"CN","word":"CROCODILE"}
+},
+{"test1":"<p style='font-size: 48px;'>GODHEAD</p>","data1":{"type":"LF","category":"AB","word":"GODHEAD"},
+"test2":"<p style='font-size: 48px;'>EWE</p>","data2":{"type":"LF","category":"CN","word":"EWE"}
+},
+{"test1":"<p style='font-size: 48px;'>EXTERMINATION</p>","data1":{"type":"LF","category":"AB","word":"EXTERMINATION"},
+"test2":"<p style='font-size: 48px;'>CLARINET</p>","data2":{"type":"LF","category":"CN","word":"CLARINET"}
+},
+{"test1":"<p style='font-size: 48px;'>FEUDALISM</p>","data1":{"type":"LF","category":"AB","word":"FEUDALISM"},
+"test2":"<p style='font-size: 48px;'>THONG</p>","data2":{"type":"LF","category":"CN","word":"THONG"}
+},
+{"test1":"<p style='font-size: 48px;'>CONJUGATION</p>","data1":{"type":"LF","category":"AB","word":"CONJUGATION"},
+"test2":"<p style='font-size: 48px;'>CREEPER</p>","data2":{"type":"LF","category":"CN","word":"CREEPER"}
+},
+{"test1":"<p style='font-size: 48px;'>BRIBE</p>","data1":{"type":"LF","category":"AB","word":"BRIBE"},
+"test2":"<p style='font-size: 48px;'>WALRUS</p>","data2":{"type":"LF","category":"CN","word":"WALRUS"}
+},
+{"test1":"<p style='font-size: 48px;'>MALADY</p>","data1":{"type":"LF","category":"AB","word":"MALADY"},
+"test2":"<p style='font-size: 48px;'>RIB</p>","data2":{"type":"LF","category":"CN","word":"RIB"}
+},
+{"test1":"<p style='font-size: 48px;'>ADVERB</p>","data1":{"type":"LF","category":"AB","word":"ADVERB"},
+"test2":"<p style='font-size: 48px;'>PROJECTOR</p>","data2":{"type":"LF","category":"CN","word":"PROJECTOR"}
+},
+{"test1":"<p style='font-size: 48px;'>PARRY</p>","data1":{"type":"LF","category":"AB","word":"PARRY"},
+"test2":"<p style='font-size: 48px;'>YOLK</p>","data2":{"type":"LF","category":"CN","word":"YOLK"}
+},
+{"test1":"<p style='font-size: 48px;'>EMBEZZLEMENT</p>","data1":{"type":"LF","category":"AB","word":"EMBEZZLEMENT"},
+"test2":"<p style='font-size: 48px;'>FAWN</p>","data2":{"type":"LF","category":"CN","word":"FAWN"}
+},
+{"test1":"<p style='font-size: 48px;'>LEVITY</p>","data1":{"type":"LF","category":"AB","word":"LEVITY"},
+"test2":"<p style='font-size: 48px;'>MOLASSES</p>","data2":{"type":"LF","category":"CN","word":"MOLASSES"}
+},
+{"test1":"<p style='font-size: 48px;'>CROAK</p>","data1":{"type":"LF","category":"AB","word":"CROAK"},
+"test2":"<p style='font-size: 48px;'>SOOT</p>","data2":{"type":"LF","category":"CN","word":"SOOT"}
+},
+{"test1":"<p style='font-size: 48px;'>DEITY</p>","data1":{"type":"LF","category":"AB","word":"DEITY"},
+"test2":"<p style='font-size: 48px;'>TIMEPIECE</p>","data2":{"type":"LF","category":"CN","word":"TIMEPIECE"}
+},
+{"test1":"<p style='font-size: 48px;'>ENUNCIATION</p>","data1":{"type":"LF","category":"AB","word":"ENUNCIATION"},
+"test2":"<p style='font-size: 48px;'>MORPHINE</p>","data2":{"type":"LF","category":"CN","word":"MORPHINE"}
+},
+{"test1":"<p style='font-size: 48px;'>REGENCY</p>","data1":{"type":"LF","category":"AB","word":"REGENCY"},
+"test2":"<p style='font-size: 48px;'>WOMB</p>","data2":{"type":"LF","category":"CN","word":"WOMB"}
+},
+{"test1":"<p style='font-size: 48px;'>WHOOP</p>","data1":{"type":"LF","category":"AB","word":"WHOOP"},
+"test2":"<p style='font-size: 48px;'>BELFRY</p>","data2":{"type":"LF","category":"CN","word":"BELFRY"}
+},
+{"test1":"<p style='font-size: 48px;'>HUE</p>","data1":{"type":"LF","category":"AB","word":"HUE"},
+"test2":"<p style='font-size: 48px;'>MOTH</p>","data2":{"type":"LF","category":"CN","word":"MOTH"}
+},
+{"test1":"<p style='font-size: 48px;'>INGRATITUDE</p>","data1":{"type":"LF","category":"AB","word":"INGRATITUDE"},
+"test2":"<p style='font-size: 48px;'>PENICILLIN</p>","data2":{"type":"LF","category":"CN","word":"PENICILLIN"}
+},
+{"test1":"<p style='font-size: 48px;'>IMPOSSIBILITY</p>","data1":{"type":"LF","category":"AB","word":"IMPOSSIBILITY"},
+"test2":"<p style='font-size: 48px;'>JEWEL</p>","data2":{"type":"LF","category":"CN","word":"JEWEL"}
+},
+]
---
+++ stimuli/test_words2.js
@@ -0,0 +1,14 @@
+testwords = [
+{"test1":"<p style='font-size: 48px;'>CARE</p>","data1":{"type":"HF","category":"AB","word":"CARE"},
+"test2":"<p style='font-size: 48px;'>LEAD</p>","data2":{"type":"HF","category":"CN","word":"LEAD"}
+},
+{"test1":"<p style='font-size: 48px;'>FUNCTION</p>","data1":{"type":"HF","category":"AB","word":"FUNCTION"},
+"test2":"<p style='font-size: 48px;'>CORNER</p>","data2":{"type":"HF","category":"CN","word":"CORNER"}
+},
+{"test1":"<p style='font-size: 48px;'>SITUATION</p>","data1":{"type":"HF","category":"AB","word":"SITUATION"},
+"test2":"<p style='font-size: 48px;'>MACHINE</p>","data2":{"type":"HF","category":"CN","word":"MACHINE"}
+},
+{"test1":"<p style='font-size: 48px;'>MAIN</p>","data1":{"type":"HF","category":"AB","word":"MAIN"},
+"test2":"<p style='font-size: 48px;'>SUN</p>","data2":{"type":"HF","category":"CN","word":"SUN"}
+},
+]
